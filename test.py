3e8eb4c979257cce0b8499e9ce68a524974b1d34
tungnlh <tungnlh.bctt@gmail.com>
2022-12-22 21:44:54 +0700

    first push

---
+++ test.py
@@ -0,0 +1,175 @@
+import argparse
+import logging
+import os
+import sys
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from tqdm import tqdm
+
+from networks.vision_transformer import SwinUnet as ViT_seg
+from networks.swin_v2 import unet_swin
+from config import get_config
+from torch.utils.data import DataLoader, random_split
+from utils.dataloader import get_loader
+from PIL import Image
+
+pred_path = '/kaggle/working/output/pred'
+os.makedirs(pred_path, exist_ok=True)
+gt_path = '/kaggle/working/output/gt'
+os.makedirs(gt_path, exist_ok=True)
+edg_path = '/kaggle/working/output/edg'
+os.makedirs(edg_path, exist_ok=True)
+fus_path = '/kaggle/working/output/fus'
+os.makedirs(fus_path, exist_ok=True)
+def eval_net(net, loader, device, n_class=1):
+    """Evaluation without the densecrf with the dice coefficient"""
+    net.eval()
+    mask_type = torch.float32 if n_class == 1 else torch.long
+    n_val = len(loader)
+    pred_idx=0
+    edg_idx=0
+    fus_idx=0
+    gt_idx=0
+
+    with tqdm(total=n_val, desc='Validation round', unit='batch', leave=False) as pbar:
+        for batch in loader:
+            imgs, true_masks = batch
+            
+            trainsize = 512 #rate        
+            imgs = F.interpolate(imgs, size=(trainsize, trainsize), mode='bilinear', align_corners=True)
+            true_masks = F.interpolate(true_masks, size=(trainsize, trainsize), mode='bilinear', align_corners=True)
+            
+            imgs = imgs.to(device=device, dtype=torch.float32)
+            true_masks = true_masks.to(device=device, dtype=mask_type)
+
+            mask_pred, edge, fused = net(imgs)
+            pred = torch.sigmoid(mask_pred)
+            pred = (pred > 0.2).float()
+            
+            edg = torch.sigmoid(edge)
+            edg = (edg > 0.2).float()
+            fus = torch.sigmoid(fused)
+            fus = (fus > 0.2).float()
+            
+            for img in pred:
+                img = img.squeeze(0).cpu().numpy()
+                img = Image.fromarray((img * 255).astype(np.uint8))
+                img.save(pred_path+'/'+str(pred_idx)+'.png')
+                pred_idx += 1
+                
+            for img in edg:
+                img = img.squeeze(0).cpu().numpy()
+                img = Image.fromarray((img * 255).astype(np.uint8))
+                img.save(edg_path+'/'+str(edg_idx)+'.png')
+                edg_idx += 1
+            for img in pred:
+                img = img.squeeze(0).cpu().numpy()
+                img = Image.fromarray((img * 255).astype(np.uint8))
+                img.save(fus_path+'/'+str(fus_idx)+'.png')
+                fus_idx += 1
+                
+            for img in true_masks:
+                img = img.squeeze(0).cpu().numpy()
+                img = Image.fromarray((img * 255).astype(np.uint8))
+                img.save(gt_path+'/'+str(gt_idx)+'.png')
+                gt_idx += 1
+
+            pbar.update()
+
+
+def test_net(net,
+              device,
+              batch_size=1,
+              n_class=1,
+              img_size=448):
+
+
+    val_img_dir = '/kaggle/working/datasets/test/images/'
+    val_mask_dir = '/kaggle/working/datasets/test/masks/'
+
+    val_loader = get_loader(val_img_dir, val_mask_dir, batchsize=batch_size, trainsize=img_size, augmentation = False)
+    net.eval()
+
+    eval_net(net, val_loader, device)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=1000,
+                        help='Number of epochs', dest='epochs')
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=1,
+                        help='Batch size', dest='batchsize')
+    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.001,
+                        help='Learning rate', dest='lr')
+    parser.add_argument('-f', '--load', dest='load', type=str, default=None,
+                        help='Load model from a .pth file')
+    parser.add_argument('-s', '--img_size', dest='size', type=int, default=448,
+                        help='The size of the images')
+    parser.add_argument('--optimizer', type=str,
+                        default='Adam', help='choosing optimizer Adam or SGD')
+    parser.add_argument('--decay_rate', type=float,
+                        default=0.1, help='decay rate of learning rate')
+    parser.add_argument('--decay_epoch', type=int,
+                        default=50, help='every n epochs decay learning rate')
+    parser.add_argument('--cfg', type=str, default='./configs/swin_tiny_patch4_window7_224_lite.yaml', 
+                        required=False, metavar="FILE", help='path to config file')
+    parser.add_argument(
+        "--opts",
+        help="Modify config options by adding 'KEY VALUE' pairs. ",
+        default=None,
+        nargs='+',
+    )
+    parser.add_argument('--zip', action='store_true', help='use zipped dataset instead of folder dataset')
+    parser.add_argument('--cache-mode', type=str, default='part', choices=['no', 'full', 'part'],
+                        help='no: no cache, '
+                                'full: cache all data, '
+                                'part: sharding the dataset into nonoverlapping pieces and only cache one piece')
+    parser.add_argument('--resume', help='resume from checkpoint')
+    parser.add_argument('--accumulation-steps', type=int, help="gradient accumulation steps")
+    parser.add_argument('--use-checkpoint', action='store_true',
+                        help="whether to use gradient checkpointing to save memory")
+    parser.add_argument('--amp-opt-level', type=str, default='O1', choices=['O0', 'O1', 'O2'],
+                        help='mixed precision opt level, if O0, no amp is used')
+    parser.add_argument('--tag', help='tag of experiment')
+    parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
+    parser.add_argument('--throughput', action='store_true', help='Test throughput only')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    args = get_args()
+    #config = get_config(args)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f'Using device {device}')
+
+    # net = UNet(128, 1)
+    # net = nn.DataParallel(net, device_ids=[0])
+    # net.to(device=device)
+    
+    #net = ViT_seg(config, img_size=args.size, num_classes=1).cuda()
+    net = unet_swin(img_size=512,size="swinv2_small_window16_256").cuda()
+    
+    if args.load:
+        net.load_state_dict(
+            torch.load(args.load, map_location=device), False
+        )
+        logging.info(f'Model loaded from {args.load}')
+
+    try:
+        test_net(net=net,
+                  batch_size=args.batchsize,
+                  device=device,
+                  img_size=args.size)
+    except KeyboardInterrupt:
+        torch.save(net.state_dict(), 'INTERRUPTED.pth')
+        logging.info('Saved interrupt')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
---
+++ train.py
@@ -0,0 +1,253 @@
+import argparse
+import logging
+import os
+import sys
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from tqdm import tqdm
+from pytorch_toolbelt import losses as L
+
+from utils.eval import eval_net
+#from lib.DS_TransUNet import UNet
+from networks.vision_transformer import SwinUnet as ViT_seg
+from networks.swin_v2 import unet_swin
+from config import get_config
+from util import BinaryDiceLoss
+
+from torch.utils.data import DataLoader, random_split
+from utils.dataloader import get_loader,test_dataset
+
+train_img_dir = '/content/datasets/train/images/'
+train_mask_dir = '/content/datasets/train/masks/'
+val_img_dir = '/content/datasets/test/images/'
+val_mask_dir = '/content/datasets/test/masks/'
+dir_checkpoint = 'models/'
+
+
+def get_logger(filename, verbosity=1, name=None):
+    level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s"
+    )
+    logger = logging.getLogger(name)
+    logger.setLevel(level_dict[verbosity])
+
+    fh = logging.FileHandler(filename, "w")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    return logger
+
+
+def cal(loader):
+    tot = 0
+    for batch in loader:
+        imgs, _ = batch
+        tot += imgs.shape[0]
+    return tot
+
+def structure_loss(pred, mask):
+    weit = 1 + 5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+    wbce = (weit*wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
+
+    pred = torch.sigmoid(pred)
+    inter = ((pred * mask)*weit).sum(dim=(2, 3))
+    union = ((pred + mask)*weit).sum(dim=(2, 3))
+    wiou = 1 - (inter + 1)/(union - inter+1)
+    return (wbce + wiou).mean()
+
+def adjust_lr(optimizer, init_lr, epoch, decay_rate=0.1, decay_epoch=30):
+    decay = decay_rate ** (epoch // decay_epoch)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] *= decay
+
+def train_net(net,
+              device,
+              epochs=500,
+              batch_size=16,
+              lr=0.01,
+              save_cp=True,
+              n_class=1,
+              img_size=448):
+
+    train_loader = get_loader(train_img_dir, train_mask_dir, batchsize=batch_size, trainsize=img_size, augmentation = False)
+    val_loader = get_loader(val_img_dir, val_mask_dir, batchsize=1, trainsize=img_size, augmentation = False)
+
+    n_train = cal(train_loader)
+    n_val = cal(val_loader)
+    logger = get_logger('Swin2Unet.log')
+
+
+    # logger.info(f'''Starting training:
+    #     Epochs:          {epochs}
+    #     Batch size:      {batch_size}
+    #     Learning rate:   {lr}
+    #     Training size:   {n_train}
+    #     Vailding size:   {n_val}
+    #     Checkpoints:     {save_cp}
+    #     Device:          {device.type}
+    #     Images size:  {img_size}
+    # ''')
+    bce_loss = nn.BCEWithLogitsLoss()
+    dice_loss = BinaryDiceLoss()
+    
+    loss_fn = L.JointLoss(first=dice_loss, second=bce_loss, first_weight=0.5, second_weight=0.5).cuda()
+    
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs//5, lr/10)
+    if n_class > 1:
+        criterion = nn.CrossEntropyLoss()
+    else:
+        criterion = nn.BCEWithLogitsLoss()
+
+
+    best_dice = 0
+    size_rates = [512]
+    for epoch in range(epochs):
+        net.train()
+
+        epoch_loss = 0
+        b_cp = False
+        Batch = len(train_loader)
+        with tqdm(total=n_train*len(size_rates), desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
+            for batch in train_loader:
+                #for rate in size_rates:
+                    imgs, true_masks = batch
+                    trainsize = 512 #rate
+                    #if rate != 448:
+                    imgs = F.interpolate(imgs, size=(trainsize, trainsize), mode='bilinear', align_corners=True)
+                    true_masks = F.interpolate(true_masks, size=(trainsize, trainsize), mode='bilinear', align_corners=True)
+
+
+                    imgs = imgs.to(device=device, dtype=torch.float32)
+                    mask_type = torch.float32 if n_class == 1 else torch.long
+                    true_masks = true_masks.to(device=device, dtype=mask_type)
+
+                    masks_pred, edge, fused = net(imgs)
+                    masks_pred = torch.squeeze(masks_pred)
+                    
+                    edge = torch.squeeze(edge)
+                    fused = torch.squeeze(fused)
+                    
+                    true_masks = torch.squeeze(true_masks)
+                    loss1 = loss_fn(masks_pred, true_masks)
+                    loss2 = loss_fn(edge, true_masks)
+                    loss3 = loss_fn(fused, true_masks)
+                    
+                    epoch_loss += (loss1.item() + loss2.item() + loss3.item())/3
+
+                    pbar.set_postfix(**{'loss (batch)': loss.item()})
+
+                    optimizer.zero_grad()
+                    loss.backward()
+                    nn.utils.clip_grad_value_(net.parameters(), 0.1)
+                    optimizer.step()
+
+                    pbar.update(imgs.shape[0])
+
+        scheduler.step()
+        val_dice = eval_net(net, val_loader, device)
+        if val_dice > best_dice:
+           best_dice = val_dice
+           b_cp = True
+        epoch_loss = epoch_loss / Batch
+        logger.info('epoch: {} train_loss: {:.3f} epoch_dice: {:.3f}, best_dice: {:.3f}'.format(epoch + 1, epoch_loss, val_dice* 100, best_dice * 100))
+
+        if save_cp and b_cp:
+            try:
+                os.mkdir(dir_checkpoint)
+                logging.info('Created checkpoint directory')
+            except OSError:
+                pass
+            torch.save(net.state_dict(),
+                       dir_checkpoint + 'epoch:{}_dice:{:.3f}.pth'.format(epoch + 1, val_dice*100))
+            logging.info(f'Checkpoint {epoch + 1} saved !')
+
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Train the model on images and target masks',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=1000,
+                        help='Number of epochs', dest='epochs')
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=16,
+                        help='Batch size', dest='batchsize')
+    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.001,
+                        help='Learning rate', dest='lr')
+    parser.add_argument('-f', '--load', dest='load', type=str, default=None,
+                        help='Load model from a .pth file')
+    parser.add_argument('-s', '--img_size', dest='size', type=int, default=448,
+                        help='The size of the images')
+    parser.add_argument('--optimizer', type=str,
+                        default='Adam', help='choosing optimizer Adam or SGD')
+    parser.add_argument('--decay_rate', type=float,
+                        default=0.1, help='decay rate of learning rate')
+    parser.add_argument('--decay_epoch', type=int,
+                        default=50, help='every n epochs decay learning rate')
+    parser.add_argument('--cfg', type=str, default='./configs/swin_tiny_patch4_window7_224_lite.yaml', 
+                        required=False, metavar="FILE", help='path to config file')
+    parser.add_argument(
+        "--opts",
+        help="Modify config options by adding 'KEY VALUE' pairs. ",
+        default=None,
+        nargs='+',
+    )
+    parser.add_argument('--zip', action='store_true', help='use zipped dataset instead of folder dataset')
+    parser.add_argument('--cache-mode', type=str, default='part', choices=['no', 'full', 'part'],
+                        help='no: no cache, '
+                                'full: cache all data, '
+                                'part: sharding the dataset into nonoverlapping pieces and only cache one piece')
+    parser.add_argument('--resume', help='resume from checkpoint')
+    parser.add_argument('--accumulation-steps', type=int, help="gradient accumulation steps")
+    parser.add_argument('--use-checkpoint', action='store_true',
+                        help="whether to use gradient checkpointing to save memory")
+    parser.add_argument('--amp-opt-level', type=str, default='O1', choices=['O0', 'O1', 'O2'],
+                        help='mixed precision opt level, if O0, no amp is used')
+    parser.add_argument('--tag', help='tag of experiment')
+    parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
+    parser.add_argument('--throughput', action='store_true', help='Test throughput only')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    args = get_args()
+    #config = get_config(args)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f'Using device {device}')
+    
+    #net = UNet(128, 1)
+    #net = nn.DataParallel(net, device_ids=[0])
+    #net = net.to(device)
+    net = unet_swin(img_size=512,size="swinv2_small_window16_256").cuda()
+    #net.load_from(config)
+    
+    if args.load:
+        net.load_state_dict(
+            torch.load(args.load, map_location=device)
+        )
+    logging.info(f'Model loaded from {args.load}')
+
+    try:
+        train_net(net=net,
+                  epochs=args.epochs,
+                  batch_size=args.batchsize,
+                  lr=args.lr,
+                  device=device,
+                  img_size=448)
+    except KeyboardInterrupt:
+        torch.save(net.state_dict(), 'INTERRUPTED.pth')
+        logging.info('Saved interrupt')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
